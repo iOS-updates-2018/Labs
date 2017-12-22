a69bf7accdb87a2f7283947a154a3089a6640b8e
Jordan Stapinski <jstapins@andrew.cmu.edu>
2017-12-22 12:32:08 -0500

    Added base file for lab 1 stopwatch app

---
+++ Lab 1/Stopwatch/Stopwatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by Jordan Stapinski on 12/22/17.
+//  Copyright © 2017 Jordan Stapinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lab 1/Stopwatch/StopwatchTests/StopwatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  StopwatchTests.swift
+//  StopwatchTests
+//
+//  Created by Jordan Stapinski on 12/22/17.
+//  Copyright © 2017 Jordan Stapinski. All rights reserved.
+//
+
+import XCTest
+@testable import Stopwatch
+
+class StopwatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
